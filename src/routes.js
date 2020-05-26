dd3290c8d16fe3f2c213ea4685de634975f1351e
unknown <heyvitoria.lopes@gmail.com>
2020-05-26 18:11:25 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Login from './pages/Login';
+import Home from './pages/Home';
+
+const AppStack = createStackNavigator();
+
+export default function Routes() {
+   return (
+      <NavigationContainer>
+         <AppStack.Screen name="Login" component={Login} />
+         <AppStack.Screen name="Home" component={Home} />
+      </NavigationContainer>
+   )
+}
